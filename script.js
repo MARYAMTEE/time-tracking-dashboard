e300b0705e681d5a403821d1a37b5cc04c3d1140
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-05-29 14:53:18 +0100

    update index, style and js

---
+++ script.js
@@ -0,0 +1,8 @@
+const dailyBtn = document.querySelector(".daily-btn");
+const weeklyBtn = document.querySelector(".weekly-btn");
+const monthlyBtn = document.querySelector(".monthlyBtn");
+const allButtons = document.querySelectorAll(".select-btn");
+
+dailyBtn.addEventListener("click", () => {
+    fetch(/)
+})
