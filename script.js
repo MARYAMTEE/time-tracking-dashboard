c6caecf94b63d22c8cb15c190806ac4b7c9c9341
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-05-29 18:37:43 +0100

    update files

--- script.js
+++
@@ -1,8 +0,0 @@
-const dailyBtn = document.querySelector(".daily-btn");
-const weeklyBtn = document.querySelector(".weekly-btn");
-const monthlyBtn = document.querySelector(".monthlyBtn");
-const allButtons = document.querySelectorAll(".select-btn");
-
-dailyBtn.addEventListener("click", () => {
-    fetch()
-})
