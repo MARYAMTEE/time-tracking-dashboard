ed62674d43c4aab3a235df8d5f6322678e3a50bd
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-05-30 16:51:02 +0100

    update active class light on button

--- script.js
+++ script.js
@@ -11,38 +11,56 @@ fetch("data.json")
     data = json;
 
     // Set up button
-    const buttons = document.querySelectorAll("button[data-time]");
-    const container = document.querySelector(".cards-container");
+const buttons = document.querySelectorAll("button[data-time]");
+const container = document.querySelector(".cards-container");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            const timeframe = button.dataset.time;
-            container.innerHTML = "";
 
-            data.forEach(item => {
-                const title = item.title;
-                const current = item.timeframes[timeframe].current;
-                const previous = item.timeframes[timeframe].previous;
+function renderCards(timeframe){
+    container.innerHTML = "";
 
-                container.innerHTML += `
-                <div class="container-card container-${title.toLowerCase()}">
-                    <div class="card card-${title.toLowerCase()}">
-                        <div class="info-top">
-                            <h3>${title}</h3>
-                            <img src="images/icon-ellipsis.svg" alt="${title} icon" />
-                        </div>
-                        <div class="info-bottom">
-                            <p>${current}hrs</p>
-                            <p>Last Week - ${previous}hrs</p>
-                        </div>
-                    </div>
+    data.forEach(item => {
+        const title = item.title;
+        const current = item.timeframes[timeframe].current;
+        const previous = item.timeframes[timeframe].previous;
+
+    container.innerHTML += `
+        <div class="container-card container-${title.toLowerCase()}">
+            <div class="card card-${title.toLowerCase()}">
+                <div class="info-top">
+                    <h3>${title}</h3>
+                    <img src="images/icon-ellipsis.svg" alt="${title} icon" />
                 </div>
-                `;
-            });
-        });
+                <div class="info-bottom">
+                    <p>${current}hrs</p>
+                    <p>Last Week - ${previous}hrs</p>
+                </div>
+            </div>
+        </div>
+     `;   
     });
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const timeframe = button.dataset.time;
+
+        //remove active class from all buttons
+        buttons.forEach(btn => btn.classList.remove("active"));
+
+        //add active class to clicked buttons
+        button.classList.add("active");
+
+        renderCards(timeframe);
+    });
+});
+
+const defaultTime = "weekly";
+renderCards(defaultTime);
+
+// Add active class to the weekly button
+document.querySelector(`button[data-time="${defaultTime}"]`).classList.add("active");
 })
 .catch(error => {
-    console.log("Fetch error:", error);
-    document.querySelector(".cards-container").innerHTML = "Failed to load data";
+    console.error("Fetch error:", error);
+    document.querySelector(".cards-container").innerHTML = "Failed to load data.";
 });
